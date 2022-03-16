1f0a702188f3a99f105d703ed173ad5c31797e39
eung7 <wannabeeung@gmail.com>
2022-03-16 20:05:38 +0900

    refreshControl 추가 및 tableViewCell register

--- GitHubRepository/RepositoryListViewController.swift
+++ GitHubRepository/RepositoryListViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class RepositoryListViewController : UITableViewController {
+    private let organization = "Apple"
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = organization + " Repositories"
+        
+        self.refreshControl = UIRefreshControl() // 당겨서 새로고침
+        let refreshControl = self.refreshControl
+        refreshControl?.backgroundColor = .systemBackground
+        refreshControl?.tintColor = .darkGray
+        refreshControl?.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+//        refreshControl?.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "RepositoryListCell",
+            for: indexPath
+        ) as? RepositoryListCell else { return UITableViewCell() }
+        
+        return cell
+    }
 }
-
-
-
-
