da52e54fabf2d9247de2a47c076d4e03773ed2a2
eung7 <wannabeeung@gmail.com>
2022-03-18 19:35:06 +0900

    rxSwift를 통해 URLResponse를 Repository 데이터로 변환시켜주는 method 구현

--- GitHubRepository/RepositoryListViewController.swift
+++ GitHubRepository/RepositoryListViewController.swift
@@ -6,9 +6,13 @@
 //
  
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RepositoryListViewController : UITableViewController {
     private let organization = "Apple"
+    private let repositories = BehaviorSubject<[Repository]>(value: [])
+    private let dispoasBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +37,57 @@ class RepositoryListViewController : UITableViewController {
     @objc func didChangedRefresh() {
         
     }
-
+    
+    // 네트워크를 통해 Json을 가져옴 -> Repository로 변환 -> BehaviorSubject로 onNext 하는 메서드
+    func fetchRepositories(of organization : String) {
+        Observable.from([organization])
+            .map { str in
+                URL(string: "https://api.github.com/orgs/\(str)/repos")!
+            }
+            .map { url -> URLRequest in
+                var request = URLRequest(url: url)
+                request.httpMethod = "GET"
+                
+                return request
+            }
+            .flatMap { request -> Observable<(response : HTTPURLResponse, data : Data)> in
+                return URLSession.shared.rx.response(request: request)
+            }
+            .filter { response, _ in
+                return 200..<300 ~= response.statusCode
+            }
+            .map { _, data -> [[String : Any]] in
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let result = json as? [[String : Any]] else {
+                    return []
+                }
+                return result
+            }
+            .filter { result in
+                result.count > 0
+            }
+            .map { objects in
+                return objects.compactMap { dic -> Repository? in
+                    guard let id = dic["id"] as? Int,
+                          let name  = dic["name"] as? String,
+                          let description = dic["description"] as? String,
+                          let stargazersCount = dic["stargazers_count"] as? Int,
+                          let language = dic["language"] as? String else {
+                        return nil
+                    }
+                    return Repository(
+                        id: id,
+                        name: name,
+                        description: description,
+                        stargazersCount: stargazersCount,
+                        language: language
+                    )
+                }
+            }
+            .subscribe(onNext : {
+                
+            })
+    }
 }
 
 
